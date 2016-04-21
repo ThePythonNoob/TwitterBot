5ecc102b432dd22e98d15c13f3d3302a33ebbd46
ThePythonNoob <thepythonnoob@gmail.com>
2016-04-20 22:06:34 -0500

    The Credentials

---
+++ credentials.py
@@ -0,0 +1,12 @@
+# Credentials for your Twitter bot account
+
+# 1. Sign into Twitter or create new account
+# 2. Make sure your mobile number is listed at twitter.com/settings/devices
+# 3. Head to apps.twitter.com and select Keys and Access Tokens
+
+CONSUMER_KEY = 'bdLqySFGnn1qWl6PqV9TxLicE'
+CONSUMER_SECRET = 'H4Jdujfxn1MRbiMLLAqWx3Rys9Aq5kq9US5qIcQRrauHkjfEgg'
+
+# Create a new Access Token
+ACCESS_TOKEN = '719248504355328001-UfUzx4lVo9LzMqWP68kjonzl5g781GO' 
+ACCESS_SECRET = '1l4aHAoRie5ToLK7V0uwv1AZWuqWHW91iaeqqugtvmx3I'
